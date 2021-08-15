083ce10d3ece6324e45b969e56eb501f1029c4ad
anhye0n <ajhrc08070@gmail.com>
2021-08-16 01:04:00 +0900

    Setting

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const path = require('path'), serveStatic = require('serve-static')
 const app = express()
 
-app.use('/ycsi', serveStatic(path.join(__dirname, 'views')))
+app.use('/', serveStatic(path.join(__dirname, 'views')))
 
-app.get('/', function (req, res){
-    res.redirect('/ycsi')
-})
+// app.get('/', function (req, res){
+//     res.redirect('/')
+// })
 
-app.listen(80, function () {
+app.listen(8088, function () {
     console.log('ycsi server is open(port: 80) http://anhye0n.me/ycsi')
 })
