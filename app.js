27617ffc1d20d258a54d1bef54526195a734cf26
anhye0n <ajhrc08070@gmail.com>
2021-08-16 12:18:27 +0900

    node 서브도메인

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, 'views')))
 
-// app.get('/', function (req, res){
-//     res.redirect('/')
-// })
-
-app.listen(8088, function () {
-    console.log('ycsi server is open(port: 8088) http://anhye0n.me:8088')
+app.get('/', function (req, res){
+    res.redirect('/')
 })
+
+/*app.listen(8088, function () {
+    console.log('ycsi server is open(port: 8088) http://anhye0n.me:8088')
+})*/
+
+module.exports = app();
