66b3e802b054378e3dd251556c75bf6db5e84111
anhye0n <ajhrc08070@gmail.com>
2021-08-30 19:16:23 +0900

    Edit

--- views/tensor/video.js
+++ views/tensor/video.js
@@ -190,6 +190,7 @@ const button = document.getElementById('my-button')
 
 function speak(num){
     button.addEventListener('click', () => {
+        cnt = 0
         console.log('num : ' + num)
         audio.src = './src/audio/' + mp3_file[num]
         setTimeout(() => { audio.play() }, 300)
